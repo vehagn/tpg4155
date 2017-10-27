9e3b1074a6aee9d67b8eb0f013c1a9d5a1ae8a46
Vegard Stenhjem Hagen <vegard.hagen@ntnu.no>
2017-10-27 12:01:31 +0200

    Added examples of romberg integration and adaptive quadrature implementations

---
+++ 07numericalIntegration/adaptiveQuadrature.m
@@ -0,0 +1,62 @@
+clear;
+f = @(x) sin(x);
+% f = @(x) x + x.^2 + x.^3 + x.^4;
+
+a =     0; % Start point
+b =    pi; % End point
+
+TOL = 1e-12;
+N = 20;
+
+h  = 0.5*(b-a);
+fa = f(a);
+fc = f(a+h);
+fb = f(b);
+
+[I, err, n] = adaptive(f,fa,fc,fb,a,b,h,TOL,simpson(fa,fc,fb,h),0,N);
+Imat = integral(f,a,b);
+
+fprintf('The integral of f(x) from %4.3f to %4.3f\n',a,b);
+fprintf('Our approximation:    %5.15e\n',I);
+fprintf('MATLAB approximation: %5.15e\n',Imat);
+fprintf('Difference:           %5.15e\n',abs(I-Imat));
+fprintf('Target error:         %5.15e\n',TOL);
+fprintf('Pessimistic error:    %5.15e\n',err);
+fprintf('Deepest recusion level was: %i\n',n);
+
+function [I, err, n] = adaptive(f,fa,fc,fb,a,b,h,TOL,I,n,N)
+    n = n+1;        % Keeping track of how deep in the recursion we are.
+    c = 0.5*(a+b);  % Midpoint of the current interval
+    h = 0.5*h;      % Halve the stepsize for the sub-intervals
+    fd = f(a+h);    % Function evaluated at midpoint of left interval
+    fe = f(a+3*h);  % Function evaluated at midpoint of right interval
+    % Full interval:      [a--d--c--e--b]
+    % Left sub interval:  [a--d--c]
+    % Right sub interval:       [c--e--b]
+    Ileft  = h*(fa+4*fd+fc)/3; % Simpson's rule left sub interval
+    Iright = h*(fc+4*fe+fb)/3; % Simpson's rule right sub interval
+    err = abs(Ileft+Iright-I); % Error estimate from theory
+    if n > N
+        % In order to avoid infinite recursion we set a deepest level the
+        % recursion is allowed to go.
+        error('Deepest level exceeded');
+    elseif err < 15*TOL % 15 comes from error analysis
+        % Richardson extrapolation gives a correcting factor of 
+        % (Ileft + Iright - I)/15 resulting in a degree of precision of 5,
+        % i.e. exact for polynomials of degree 5 or less.
+        I = Ileft+Iright + (Ileft + Iright - I)/15;
+        return;
+    else
+        % If the desired accuracy is not obtained we further sub-divide the
+        % interval using a recursive relation.
+        [Ileft , Lerr, Li] = adaptive(f,fa,fd,fc,a,c,h,TOL/2,Ileft ,n,N);
+        [Iright, Rerr, Ri] = adaptive(f,fc,fe,fb,c,b,h,TOL/2,Iright,n,N);
+        I   = Ileft + Iright;   % Summing the left and right integral
+        err = max(Lerr,Rerr);   % Pessimistic error estimate
+        n   = max(Li,Ri);       % Pick the deepest level
+    end
+end
+
+function I = simpson(fa,fc,fb,h)
+    I = h*(fa+4*fc+fb)/3;
+end
---
+++ 07numericalIntegration/rombergIntegration.m
@@ -0,0 +1,35 @@
+clear;
+f = @(x) sin(x);
+
+a =  0; % Start point
+b = pi; % End point
+n = 20; % Number of nodes
+
+R = romberg(f,n,a,b);
+
+fprintf('Romberg integration: %8.7f\n',R(n,n));
+
+function R = romberg(f,n,a,b)
+%Performs naive Romberg integration. 
+    R = zeros(n,n); % We are not concerned about memory
+    m = 1;          % Starting number of intervals
+    h = (b-a)/m;    % Step length
+    R(1,1) =  0.5*h*(f(a)-f(b));
+    for k = 2:n
+        % Calculate the first column of R.
+        % This is the only part that requires function evaluations.
+        h = 0.5*h; % halve the step length
+        r = 0;
+        for i = 1:m
+           r = r + f(a+(2*i-1)*h);
+        end
+        R(k,1) = 0.5*R(k-1) + h*r;
+        m = 2*m; % Double the number of intervals
+    end
+    % Calculate the higher orders using recurssion.
+    for j = 2:n
+        for k = j:n
+            R(k,j) = ((4^j)*R(k,j-1)-R(k-1,j-1))/((4^j)-1);
+        end
+    end       
+end
