c5094614c6aba8f8ff74745b38aa8f3d17dff753
Vegard Stenhjem Hagen <vegard.hagen@ntnu.no>
2018-09-06 10:10:07 +0200

    Added a naïve version of the Gauss-Seidel algorithm.

--- 02linSysIteration/linSysIteration.m
+++ 02linSysIteration/linSysIteration.m
@@ -9,25 +9,25 @@ x0 = [1; 2; 2];
 xT = [2;4;3];
 
 N = 10;
-x = x0;
 
 checkConvergence(A);
 
+x = x0;
+fprintf('Jacobi:\n');
 fprintf('It: %2i\tx_k: %7.6f\ty_k: %7.6f\tz_k: %7.6f\t||x^k-x^T|| %1.4e\n',...
     0, x, sqrt(sum((x - xT).^2)));
-fprintf('Gauss-Seidel:\n');
 for k = 1:N
-    x = gaussSeidel(A,b,x);
+    x = jacobi(A,b,x);
     fprintf('It: %2i\tx_k: %7.6f\ty_k: %7.6f\tz_k: %7.6f\t||x^k-x^T|| %1.4e\n',...
         k, x, sqrt(sum((x - xT).^2)));
 end
 
-
-N = 10;
-x = x0;
-fprintf('Jacobi:\n');
+x = x0; % reset starting vector
+fprintf('Gauss-Seidel:\n');
+fprintf('It: %2i\tx_k: %7.6f\ty_k: %7.6f\tz_k: %7.6f\t||x^k-x^T|| %1.4e\n',...
+    0, x, sqrt(sum((x - xT).^2)));
 for k = 1:N
-    x = jacobi(A,b,x);
+    x = gaussSeidel2(A,b,x);
     fprintf('It: %2i\tx_k: %7.6f\ty_k: %7.6f\tz_k: %7.6f\t||x^k-x^T|| %1.4e\n',...
         k, x, sqrt(sum((x - xT).^2)));
 end
@@ -47,18 +47,29 @@ function checkConvergence(A)
     end
 end
 
-function x = gaussSeidel(A,b,x)
-    % We don't need to "split" into (k) and (k-1) since the new
-    % values are stored over the old values. This makes for cleaner code.
-    for i = 1:length(x)
-        % We include j==i in the sum (which we shouldn't), 
-        % so we have to subtract it.
-        x(i) = x(i) - (A(i,:)*x - b(i))/A(i,i) ;
-    end
-end
-
 function x = jacobi(A,b,x)
     % Jacobi iteration on component form. Written like this we use all
     % the old values stored in x before overwriting them.
     x = x - (A*x - b)./diag(A) ;
 end
+
+function xO = gaussSeidel1(A,b,x)
+    % Naïve implementation of the Gauss-Seidel algorithm
+    xO = x;
+    for i = 1:length(x)
+       x(i) = (b(i) - sum(A(i,1:i-1)*x(1:i-1)) - sum(A(i,i+1:end)*xO(i+1:end)))/A(i,i);
+    end
+    xO = x;
+end
+
+function x = gaussSeidel2(A,b,x)
+    % We don't need to "split" into (k) and (k-1) since the new
+    % values are stored over the old values. This makes for cleaner code.
+    for i = 1:length(x)
+        % We include the diagonal elements (j==i) in the sum, which we 
+        % shouldn't. We therefore have to subtract the diagonal elements.
+        % As an added bonus this results in cleaner code.
+        x(i) = x(i) + (b(i) - A(i,:)*x)/A(i,i) ;
+    end
+end
+
